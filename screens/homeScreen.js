ceb218daef185ec23279a33a5b39c6877fe53dd7
yeonjae <pyj3740@gmail.com>
2025-06-05 11:01:35 +0900

    ✨ Feat: create initial homeScreen
    홈 초기 화면 생성

---
+++ screens/homeScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {},
+});
