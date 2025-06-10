408d45bdf6b6fb7b5a547b4a532c30890ba7ef8f
yeonjae <pyj3740@gmail.com>
2025-06-10 15:27:04 +0900

    ✨ Feat: add Mypage screen to navigation stack
    - App.js에 마이페이지 스크린을 Stack.Navigator에 추가하였습니다
    - BottonTab 연동 및 탭 이동 기능 구현

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen/index";
 import ChatListScreen from "./screens/chatListScreen";
 import CharacterDetailScreen from "./screens/characterDetailScreen";
+import MyPageScreen from "./screens/myPageScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -23,6 +24,7 @@ export default function App() {
             name="CharacterDetail"
             component={CharacterDetailScreen}
           />
+          <Stack.Screen name="Mypage" component={MyPageScreen} />
         </Stack.Navigator>
         <StatusBar style="light" backgroundColor="#1a1b1b" />
       </NavigationContainer>
