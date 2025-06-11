252837a61431572d109fb22e85d981bb4e6c4738
yeonjae <pyj3740@gmail.com>
2025-06-11 13:50:21 +0900

    ✨ Feat: add Keyboard handing for ios and Android
    키보드 동작을 자연스럽게 처리하기 위해 KeyBoardAvoidingView와 TouchableWithoutFeedback을 적용

--- screens/SignUpScreen/signUpStep1.js
+++ screens/SignUpScreen/signUpStep1.js
@@ -4,6 +4,10 @@ import {
   Text,
   TouchableOpacity,
   TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Ionicons from "@expo/vector-icons/Ionicons";
@@ -19,120 +23,133 @@ function SignUpStep1() {
     username.length > 0 && password.length > 0 && passwordConfirm.length > 0;
 
   return (
-    <SafeAreaView style={styles.container}>
-      {/* 헤더 */}
-      <View style={styles.header}>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Ionicons name="chevron-back-sharp" size={24} color="white" />
-        </TouchableOpacity>
-        <Text style={{ color: "white", fontSize: 18, fontWeight: "500" }}>
-          회원가입
-        </Text>
-        <View>
-          <Ionicons name="chevron-back-sharp" size={24} color="#1a1b1b" />
-        </View>
-      </View>
-
-      {/* 입력 내용 */}
-      <View style={{ flex: 1, marginHorizontal: 15, marginTop: 20 }}>
-        <View style={{ marginTop: 30 }}>
-          {/* 아이디 입력 */}
-          <View style={{ marginBottom: 15 }}>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <SafeAreaView style={styles.container}>
+          {/* 헤더 */}
+          <View style={styles.header}>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Ionicons name="chevron-back-sharp" size={24} color="white" />
+            </TouchableOpacity>
             <Text style={{ color: "white", fontSize: 18, fontWeight: "500" }}>
-              아이디
+              회원가입
             </Text>
-            <TextInput
-              placeholder="아이디"
-              placeholderTextColor="rgba(225, 225,225, 0.5)"
-              value={username}
-              onChangeText={(text) => setUsername(text)}
-              style={{
-                backgroundColor: "rgb(45,45,45)",
-                padding: 10,
-                marginVertical: 10,
-                borderRadius: 6,
-                borderWidth: 0.3,
-                borderColor: "rgba(225, 225,225, 0.3)",
-                fontSize: 16,
-                color: "white",
-              }}
-            />
+            <View>
+              <Ionicons name="chevron-back-sharp" size={24} color="#1a1b1b" />
+            </View>
           </View>
 
-          {/* 패스워드 입력 */}
-          <View style={{ marginBottom: 15 }}>
-            <Text style={{ color: "white", fontSize: 18, fontWeight: "500" }}>
-              패스워드
-            </Text>
-            <TextInput
-              placeholder="패스워드"
-              placeholderTextColor="rgba(225, 225,225, 0.5)"
-              secureTextEntry={true} // 패스워드 입력 모드
-              value={password}
-              onChangeText={(text) => setPassword(text)}
-              style={{
-                backgroundColor: "rgb(45,45,45)",
-                padding: 10,
-                marginVertical: 10,
-                borderRadius: 6,
-                borderWidth: 0.3,
-                borderColor: "rgba(225, 225,225, 0.3)",
-                fontSize: 16,
-                color: "white",
-              }}
-            />
+          {/* 입력 내용 */}
+          <View style={{ flex: 1, marginHorizontal: 15, marginTop: 20 }}>
+            <View style={{ marginTop: 30 }}>
+              {/* 아이디 입력 */}
+              <View style={{ marginBottom: 15 }}>
+                <Text
+                  style={{ color: "white", fontSize: 18, fontWeight: "500" }}
+                >
+                  아이디
+                </Text>
+                <TextInput
+                  placeholder="아이디"
+                  placeholderTextColor="rgba(225, 225,225, 0.5)"
+                  value={username}
+                  onChangeText={(text) => setUsername(text)}
+                  style={{
+                    backgroundColor: "rgb(45,45,45)",
+                    padding: 10,
+                    marginVertical: 10,
+                    borderRadius: 6,
+                    borderWidth: 0.3,
+                    borderColor: "rgba(225, 225,225, 0.3)",
+                    fontSize: 16,
+                    color: "white",
+                  }}
+                />
+              </View>
+
+              {/* 패스워드 입력 */}
+              <View style={{ marginBottom: 15 }}>
+                <Text
+                  style={{ color: "white", fontSize: 18, fontWeight: "500" }}
+                >
+                  패스워드
+                </Text>
+                <TextInput
+                  placeholder="패스워드"
+                  placeholderTextColor="rgba(225, 225,225, 0.5)"
+                  secureTextEntry={true} // 패스워드 입력 모드
+                  value={password}
+                  onChangeText={(text) => setPassword(text)}
+                  style={{
+                    backgroundColor: "rgb(45,45,45)",
+                    padding: 10,
+                    marginVertical: 10,
+                    borderRadius: 6,
+                    borderWidth: 0.3,
+                    borderColor: "rgba(225, 225,225, 0.3)",
+                    fontSize: 16,
+                    color: "white",
+                  }}
+                />
+              </View>
+
+              {/* 패스워드 확인 입력 */}
+              <View style={{ marginBottom: 15 }}>
+                <Text
+                  style={{ color: "white", fontSize: 18, fontWeight: "500" }}
+                >
+                  패스워드 확인
+                </Text>
+                <TextInput
+                  placeholder="패스워드 확인"
+                  placeholderTextColor="rgba(225, 225,225, 0.5)"
+                  secureTextEntry={true} // 패스워드 입력 모드
+                  value={passwordConfirm}
+                  onChangeText={(text) => setPasswordConfirm(text)}
+                  style={{
+                    backgroundColor: "rgb(45,45,45)",
+                    padding: 10,
+                    marginVertical: 10,
+                    borderRadius: 6,
+                    borderWidth: 0.3,
+                    borderColor: "rgba(225, 225,225, 0.3)",
+                    fontSize: 16,
+                    color: "white",
+                  }}
+                />
+              </View>
+            </View>
           </View>
 
-          {/* 패스워드 확인 입력 */}
-          <View style={{ marginBottom: 15 }}>
-            <Text style={{ color: "white", fontSize: 18, fontWeight: "500" }}>
-              패스워드 확인
-            </Text>
-            <TextInput
-              placeholder="패스워드 확인"
-              placeholderTextColor="rgba(225, 225,225, 0.5)"
-              secureTextEntry={true} // 패스워드 입력 모드
-              value={passwordConfirm}
-              onChangeText={(text) => setPasswordConfirm(text)}
-              style={{
-                backgroundColor: "rgb(45,45,45)",
-                padding: 10,
-                marginVertical: 10,
-                borderRadius: 6,
-                borderWidth: 0.3,
-                borderColor: "rgba(225, 225,225, 0.3)",
-                fontSize: 16,
-                color: "white",
-              }}
-            />
+          {/*  다음 버튼 */}
+          <View style={{ marginHorizontal: 20 }}>
+            <TouchableOpacity
+              style={{ alignItems: "center" }}
+              disabled={!isFormFilled} // 입력이 안 되면 비활성화
+            >
+              <Text
+                style={{
+                  backgroundColor: isFormFilled
+                    ? "rgb(103, 40, 225)"
+                    : "rgb(45, 45, 45)",
+                  color: isFormFilled ? "white" : "rgb(115, 120, 131)",
+                  paddingVertical: 15,
+                  borderRadius: 6,
+                  fontSize: 16,
+                  width: "100%",
+                  textAlign: "center",
+                }}
+              >
+                다음
+              </Text>
+            </TouchableOpacity>
           </View>
-        </View>
-      </View>
-
-      {/*  다음 버튼 */}
-      <View style={{ marginHorizontal: 20 }}>
-        <TouchableOpacity
-          style={{ alignItems: "center" }}
-          disabled={!isFormFilled} // 입력이 안 되면 비활성화
-        >
-          <Text
-            style={{
-              backgroundColor: isFormFilled
-                ? "rgb(103, 40, 225)"
-                : "rgb(45, 45, 45)",
-              color: isFormFilled ? "white" : "rgb(115, 120, 131)",
-              paddingVertical: 15,
-              borderRadius: 6,
-              fontSize: 16,
-              width: "100%",
-              textAlign: "center",
-            }}
-          >
-            다음
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+        </SafeAreaView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
