d44e54f6af23ffc06fff6d79523076e19882e5b2
yeonjae <pyj3740@gmail.com>
2025-06-14 15:46:15 +0900

    ✨ Feat: add EditProfile page
    프로필 수정 페이지 추가
    - Stack.Screen 등록
    - 마이페이지에서 프로필 수정 페이지 연결

---
+++ screens/editProfile.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function EditProfile() {
+  return (
+    <View>
+      <Text>프로필 편집</Text>
+    </View>
+  );
+}
+
+export default EditProfile;
+
+const styles = StyleSheet.create({});
