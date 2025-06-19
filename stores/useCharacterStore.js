d2943bba4cb66bc540e53c0d8dc198f1965a0d8f
yeonjae <pyj3740@gmail.com>
2025-06-19 17:50:19 +0900

    ✨ Feat: add character global state using zustand
    캐릭터 생성시 전송될 내용을 관리하는 전역 변수 생성

---
+++ stores/useCharacterStore.js
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+
+const useCharacterStore = create((set) => ({
+  title: "", // 제목
+  description: "", // 상세설명
+  character_image: null, // 캐릭터 이미지
+  name: "", // 이름
+  character_info: "", // 설명
+  intro: [], // 첫 상황
+  example_situation: [], // 상황 예시
+  presentation: "", // 소개글?
+  // 해시태그
+  // 크리에이터 코멘트
+
+  setTitle: (newTitle) => set({ title: newTitle }),
+  setDescription: (newDescription) => set({ description: newDescription }),
+  setName: (newName) => set({ name: newName }),
+  setCharacterInfo: (newCharacterInfo) =>
+    set({ character_info: newCharacterInfo }),
+
+  // 내용 초기화 함수
+  resetCharacter: () =>
+    set({
+      title: "", // 제목
+      description: "", // 상세설명
+      character_image: null, // 캐릭터 이미지
+      name: "", // 이름
+      character_info: "", // 설명
+      intro: [], // 첫 상황
+      example_situation: [], // 상황 예시
+      presentation: "", // 소개글?
+      // 해시태그
+      // 크리에이터 코멘트
+    }),
+}));
+
+export default useCharacterStore;
