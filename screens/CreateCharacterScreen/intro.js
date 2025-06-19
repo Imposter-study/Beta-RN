d22abd4bccd7e9cef9052a8bd10f4a66d1786f33
yeonjae <pyj3740@gmail.com>
2025-06-19 19:26:53 +0900

    ✨ Feat: enable input only when character name is provided
    - 캐릭터 이름이 입력된 경우에만 입력이 가능하도록 추가
    - 이름이 없는 경우 안내 문구가 표시되고, 이름이 있을 경우 입력창이 활성화

--- screens/CreateCharacterScreen/intro.js
+++ screens/CreateCharacterScreen/intro.js
@@ -1,6 +1,18 @@
-import { StyleSheet, View, Text, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import useCharacterStore from "../../stores/useCharacterStore";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 
 function Intro() {
+  const { name } = useCharacterStore();
+
   return (
     <>
       {/* 글자수 */}
@@ -42,15 +54,111 @@ function Intro() {
           borderTopRightRadius: 16,
         }}
       >
-        <Text
-          style={{
-            color: "#ffffff80",
-            fontSize: 16,
-            padding: 12,
-          }}
-        >
-          캐릭터의 이름을 먼저 입력해주세요
-        </Text>
+        {!name ? (
+          <Text
+            style={{
+              color: "#ffffff80",
+              fontSize: 16,
+              padding: 12,
+            }}
+          >
+            캐릭터의 이름을 먼저 입력해주세요
+          </Text>
+        ) : (
+          <View style={{ padding: 12 }}>
+            <View
+              style={{
+                borderBottomWidth: 1,
+                borderBottomColor: "#ffffff0d",
+              }}
+            >
+              <View style={{ flexDirection: "row", gap: 10 }}>
+                <TouchableOpacity
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    gap: 5,
+                    paddingBottom: 12,
+                    borderBottomColor: "white",
+                    borderBottomWidth: 1.5,
+                  }}
+                >
+                  {/* 이미지 자리 */}
+                  <Feather name="zap" size={24} color="black" />
+                  <Text style={{ color: "#ffffff80" }}>{name}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    gap: 5,
+                    paddingBottom: 12,
+                  }}
+                >
+                  <Feather name="eye" size={24} color="black" />
+                  <Text style={{ color: "white" }}>유저</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <TextInput
+              placeholder={`${name}의 메세지 입력`}
+              placeholderTextColor="#9ca3af"
+              multiline={true}
+              style={{
+                color: "white",
+                fontSize: 16,
+                fontWeight: "400",
+                marginTop: 10,
+                padding: 10,
+                minHeight: 70,
+                maxHeight: 124,
+              }}
+            />
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
+            >
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    color: "white",
+                    backgroundColor: "rgb(62 62 65)",
+                    borderRadius: 18,
+                    paddingVertical: 7,
+                    paddingHorizontal: 10,
+                    alignSelf: "flex-start",
+                  }}
+                >
+                  {"{{"}user{"}}"}
+                </Text>
+              </TouchableOpacity>
+              <View
+                style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
+              >
+                <TouchableOpacity>
+                  <FontAwesome6
+                    name="asterisk"
+                    size={18}
+                    color="#ffffff80"
+                    style={{ padding: 10 }}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Feather
+                    name="arrow-up"
+                    size={24}
+                    color="rgb(115 120 131)"
+                    style={{
+                      backgroundColor: "rgb(45 45 45)",
+                      paddingHorizontal: 10,
+                      paddingVertical: 5,
+                      borderRadius: 100,
+                    }}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        )}
       </View>
     </>
   );
