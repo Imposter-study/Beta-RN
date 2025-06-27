ff3a757f311f66eb9b790d6744a2f9eba87f1f0a
yeonjae <pyj3740@gmail.com>
2025-06-27 13:07:43 +0900

    ✨ Feat: create zustand store for signup form
    - 회원가입 입력값 상태 관리를 위한 zustand store 생성
    - username, password, birth_date, gender 등 필드 포함
    - set 메서드와 초기화 메서드(setClear) 구현

---
+++ stores/useSignupStore.js
@@ -0,0 +1,29 @@
+import { create } from "zustand";
+
+const useSignupStore = create((set) => ({
+  username: "",
+  password: "",
+  password_confirm: "",
+  birth_date: "",
+  gender: "",
+  introduce: "",
+
+  setUsername: (newUsername) => set({ username: newUsername }),
+  setPassword: (newPassword) => set({ password: newPassword }),
+  setPasswordConfirm: (newPasswordConfirm) =>
+    set({ password_confirm: newPasswordConfirm }),
+  setBirthDate: (newBirthdate) => set({ birth_date: newBirthdate }),
+  setGender: (newGender) => set({ gender: newGender }),
+
+  setClear: () =>
+    set({
+      username: "",
+      password: "",
+      password_confirm: "",
+      birth_date: "",
+      gender: "",
+      introduce: "",
+    }),
+}));
+
+export default useSignupStore;
