48facd5c505a32871b0375967fae65aaf3a2dc6b
yeonjae <pyj3740@gmail.com>
2025-06-28 05:35:18 +0900

    ✨ Feat: handle kakao login response with conditional navigation
    - 소셜 로그인 응답에 따라 is_signup 여부로 분기 처리 로직 추가
    - 회원가입이 완료된 유저는 홈 화면으로 이동하며, 그렇지 않은 경우 닉네임과 토큰을 저장하고 회원가입 스텝으로 이동

--- components/kakaoLoginScreen.js
+++ components/kakaoLoginScreen.js
@@ -6,12 +6,14 @@ import axios from "axios";
 import qs from "qs";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as SecureStore from "expo-secure-store";
+import { useSocialSignupStroe } from "../stores/useSignupStore";
 
 export default function KakaoLoginScreen() {
   const navigation = useNavigation();
   const webviewRef = useRef(null);
   const [webviewKey, setWebviewKey] = useState(0);
   const [usedCode, setUsedCode] = useState(null); // 중복 요청 방지
+  const { setNickname, setKey } = useSocialSignupStroe();
 
   const authUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_CLIENT_ID}&redirect_uri=${encodeURIComponent(
     REDIRECT_URI
@@ -46,11 +48,31 @@ export default function KakaoLoginScreen() {
 
         const loginRes = await axios.post(BACKEND_LOGIN_URL, { access_token });
         console.log("백엔드 응답:", loginRes.data);
-        const { key } = loginRes.data;
-        // 토큰을 저장
-        await SecureStore.setItemAsync("access_token", key);
-        // 로그인 성공 시 홈으로 이동
-        navigation.replace("Home");
+
+        const { is_signup } = loginRes.data; // 소셜로그인으로 회원가입이 완료된 유저인지 확인
+        if (is_signup) {
+          const { key } = loginRes.data;
+          console.log(key);
+          // 토큰을 저장
+          await SecureStore.setItemAsync("access", key);
+          // 로그인 성공 시 홈으로 이동
+          navigation.replace("Home");
+        } else {
+          const { kakao_id, nickname, token } = loginRes.data;
+          console.log(
+            "kakao_id :",
+            kakao_id,
+            "\nnickname :",
+            nickname,
+            "\ntoken :",
+            token
+          );
+          setNickname(nickname);
+          setKey(token);
+          navigation.navigate("SignUp", {
+            screen: "SignUpStep2",
+          });
+        }
       }
     } catch (error) {
       console.error("로그인 에러:", error?.response?.data || error.message);
